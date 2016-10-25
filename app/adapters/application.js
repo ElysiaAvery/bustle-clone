f281d121bfdc70225313f029755ca684b3cc3f81
Elysia Nason and Josh Huffman <elysia+josh@>
2016-10-25 14:45:42 -0700

    add delete functionality and sticky header

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,9 @@
 import FirebaseAdapter from 'emberfire/adapters/firebase';
 
 export default FirebaseAdapter.extend({
+  myStickyOptions: {
+    topSpacing: 0,
+    responsiveWidth: false,
+    zIndex: 2,
+  },
 });
